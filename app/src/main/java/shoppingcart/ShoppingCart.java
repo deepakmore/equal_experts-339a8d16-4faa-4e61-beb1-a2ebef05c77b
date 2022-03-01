8e875114319184121fa2611fc66a317a0d94d2c5
Deepak More <deepakmore@outlook.com>
2022-03-02 00:17:43 +0530

    Code REfactoring

--- app/src/main/java/shoppingcart/ShoppingCart.java
+++ app/src/main/java/shoppingcart/ShoppingCart.java
@@ -12,26 +12,20 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
-import static shoppingcart.constant.ShoppingCartConstants.HUNDRED;
-import static shoppingcart.constant.ShoppingCartConstants.PRECISION_TO_ROUND_OFF;
-import static shoppingcart.constant.ShoppingCartConstants.SALES_TAX;
-import static shoppingcart.constant.ShoppingCartConstants.ZERO;
+import static shoppingcart.constant.ShoppingCartConstants.*;
 
 public class ShoppingCart {
 
     private final Map<String, ProductAndQuantity> cart;
-    private BigDecimal totalPrice;
 
     public ShoppingCart() {
         cart = new LinkedHashMap<>();
-        totalPrice = ZERO;
     }
 
     public void addProduct(Product product, int expectedQuantity) {
         if(expectedQuantity > 0) {
             int totalQuantity = cart.containsKey(product.getName()) ? cart.get(product.getName()).getQuantity() + expectedQuantity : expectedQuantity;
             cart.put(product.getName(), new ProductAndQuantity(product, totalQuantity));
-            calculateTotalPrice(product, expectedQuantity);
             return;
         }
 
@@ -39,19 +33,20 @@ public class ShoppingCart {
     }
 
     public BigDecimal totalPrice() {
-        if(cart.size() >= 1) {
+            BigDecimal totalPrice = calculateTotalPrice();
             BigDecimal tax = applySalesTax(totalPrice);
             return NumberUtil.valueAfterRoundOff(totalPrice.add(tax), PRECISION_TO_ROUND_OFF);
-        }
-        return ZERO;
     }
 
-    private void calculateTotalPrice(Product product, int expectedQuantity) {
-        totalPrice = totalPrice.add(BigDecimal.valueOf(product.getPrice() * expectedQuantity));
+    private BigDecimal calculateTotalPrice() {
+        return NumberUtil.valueAfterRoundOff(
+                BigDecimal.valueOf(cart.values().stream()
+                .mapToDouble( v -> v.getProduct().getPrice() * v.getQuantity())
+                .sum()), PRECISION_TO_ROUND_OFF);
     }
 
     protected BigDecimal applySalesTax(BigDecimal amountWithoutTax) {
-        return (amountWithoutTax.multiply(SALES_TAX)).divide(HUNDRED, BigDecimal.ROUND_UP);
+        return NumberUtil.valueAfterRoundOff((amountWithoutTax.multiply(SALES_TAX)).divide(HUNDRED, BigDecimal.ROUND_UP), PRECISION_TO_ROUND_OFF);
     }
 
     public Set<ProductAndQuantity> getCartItems() {
--- app/src/main/java/shoppingcart/constant/ShoppingCartConstants.java
+++ app/src/main/java/shoppingcart/constant/ShoppingCartConstants.java
@@ -6,5 +6,5 @@ public class ShoppingCartConstants {
     public static int PRECISION_TO_ROUND_OFF = 2;
     public static BigDecimal SALES_TAX = BigDecimal.valueOf(12.5);
     public static BigDecimal HUNDRED = BigDecimal.valueOf(100);
-    public static BigDecimal ZERO = BigDecimal.valueOf(0.00);
+    public static BigDecimal ZERO = new BigDecimal("0.00");
 }
